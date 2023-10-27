007596654d25760f638db087f93b924e6e8133b8
Meefx <muhfakhri35@gmail.com>
2023-10-27 20:20:01 +0700

    update: rekomendasi 50%

--- rekomendasi/models.py
+++ rekomendasi/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from book.models import Book
 
 # Create your models here.
+class BookRecommendation(models.Model):
+    recommended_book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    source_book = models.ForeignKey(Book, related_name='recommendations', on_delete=models.CASCADE)
+    recommendation_score = models.IntegerField()
+
+    def __str__(self):
+        return f"Rekomendasi {self.recommended_book.title} dari {self.source_book.title}"
