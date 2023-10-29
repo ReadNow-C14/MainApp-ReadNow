866f4fb33564f79cca634c00333fa08b5d22c5e0
fadeeaal <rakha.fadil@ui.ac.id>
2023-10-29 11:29:46 +0700

    review feature update (unstyled)

--- review_buku/urls.py
+++ review_buku/urls.py
@@ -4,5 +4,5 @@ from review_buku.views import *
 urlpatterns = [
     path('<int:book_id>/', reviews_for_book, name='reviews_for_book'),
     path('submit_review/<int:book_id>/', submit_review, name='submit_review'),
-    path('review_json/', review_json, name='review_json'),
+    path('review_json/<int:book_id>/', review_json, name='review_json'),
 ]
