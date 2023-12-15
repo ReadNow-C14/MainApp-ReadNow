098558ebd7b6c5ff38ac4a9ee882c52f957a1ad3
Fadeeaal <rakha.fadil@ui.ac.id>
2023-12-15 15:50:06 +0700

    test

--- review_buku/views.py
+++ review_buku/views.py
@@ -11,19 +11,23 @@ from django.contrib import messages
 from datetime import datetime
 import json
 
+
 def reviews_for_book(request, book_id):
     book = Book.objects.get(id=book_id)
     reviews = Review.objects.filter(book=book)
     form = ReviewForm()
     return render(request, 'review_buku.html', {'reviews': reviews, 'book': book, 'form': form})
 
+
 def review_json(request):
     data = Review.objects.all()
     return HttpResponse(serializers.serialize('json', data))
 
+
 def user_json(request):
     data = User.objects.all()
     return HttpResponse(serializers.serialize('json', data))
+
 
 @login_required(login_url='/login/')
 @csrf_exempt
@@ -31,7 +35,8 @@ def submit_review(request, book_id):
     url = request.META.get('HTTP_REFERER')
     if request.method == 'POST':
         try:
-            reviews = Review.objects.get(user__id=request.user.id, book__id=book_id)
+            reviews = Review.objects.get(
+                user__id=request.user.id, book__id=book_id)
             form = ReviewForm(request.POST, instance=reviews)
         except Review.DoesNotExist:
             form = ReviewForm(request.POST)
@@ -44,13 +49,15 @@ def submit_review(request, book_id):
             return redirect(url)
         else:
             return redirect(url)
-        
+
+
 @login_required(login_url='/login/')
 @csrf_exempt
 def submit_review_ajax(request, book_id):
     if request.method == 'POST':
         try:
-            reviews = Review.objects.get(user__id=request.user.id, book__id=book_id)
+            reviews = Review.objects.get(
+                user__id=request.user.id, book__id=book_id)
             form = ReviewForm(request.POST, instance=reviews)
         except Review.DoesNotExist:
             form = ReviewForm(request.POST)
@@ -60,42 +67,37 @@ def submit_review_ajax(request, book_id):
             review.user = request.user
             review.book_id = book_id
             review.save()
-            
+
             return HttpResponse(b"CREATED", status=201)
         else:
             messages.error(request, 'There was an error with your submission.')
             return HttpResponseNotFound()
-        
-def get_review_json(request, book_id):
-    book = Book.objects.get(pk = book_id)
-    data_review = Review.objects.filter(book=book)
-    data = [{'id_buk_db': review.book.pk,
-            'user': review.user.username,
-            'comment': review.comment,
-            'created_at': datetime.fromisoformat(str(review.created_at)).strftime("%d-%m-%y %H:%M:%S"),
-            'rating': review.rating, }
-            for review in data_review]
-    return JsonResponse(data,safe=False)
 
-@login_required(login_url='/login/')
+
+def get_review_json(request, book_id):
+    book = Book.objects.get(pk=book_id)
+    data_review = Review.objects.filter(book=book)
+    data = [{'book': review.book.pk,
+            'user': review.user.username,
+             'comment': review.comment,
+             'created_at': datetime.fromisoformat(str(review.created_at)).strftime("%d-%m-%y %H:%M:%S"),
+             'rating': review.rating, }
+            for review in data_review]
+    return JsonResponse(data, safe=False)
+
+
 @csrf_exempt
 def submit_review_flutter(request, book_id):
     if request.method == 'POST':
-        data = json.loads(request.body)
-        try:
-            reviews = Review.objects.get(user__id=data['user_id'], book__id=book_id)
-            form = ReviewForm(data, instance=reviews)
-        except Review.DoesNotExist:
-            form = ReviewForm(data)
+        data = json.loads(request.body.decode('utf-8'))
 
-        if form.is_valid():
-            review = form.save(commit=False)
-            review.user = data['user']
-            review.book_id = book_id
-            review.save()
-            
-            # Return a success response in JSON format
-            return JsonResponse({'message': 'Review submitted successfully'}, status=201)
-        else:
-            # Return an error response in JSON format
-            return JsonResponse({'error': 'Form submission invalid'}, status=400)
+        # Konversi createdAt dari string ke datetime
+        review = Review.objects.create(
+            user=data['user'],
+            book=data['book'],
+            comment=data['comment'],
+            rating=data['rating'],
+            createdAt=datetime.datetime.now()
+        )
+        review.save()
+        return JsonResponse({'message': 'Review submitted successfully'}, status=201)
