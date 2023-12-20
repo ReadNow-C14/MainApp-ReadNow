c8a220f6e688700510ecf2eafe164b2c56ea06e4
hrjuno <harjunoabdullah@gmail.com>
2023-12-20 10:22:37 +0700

    edit pinjam buku

--- pinjam_buku/urls.py
+++ pinjam_buku/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('borrow-book-ajax/<int:id>', borrow_book_ajax, name='borrow_book_ajax'),
     path('borrowed-books/', borrowed_books, name='borrowed_books'),
     path('return-book-ajax/<int:id>', return_book_ajax, name='return_book_ajax'),
-    path('borrow-book-flutter/<int:id>', borrow_book_flutter, name='borrow_book_flutter'),
+    path('borrow-book-flutter/<int:book_id>/', borrow_book_flutter, name='borrow_book_flutter'),
+    path('return-borrowed-book-flutter/<int:book_id>/', return_borrowed_book_flutter, name='return_borrowed_book_flutter'),
+    path('borrow-flutter/<int:book_id>/', borrow_flutter, name='borrow_flutter'),
 ]
